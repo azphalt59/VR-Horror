7d98c280194b5069d9f708dbfe2bfaca2053c4b7
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-09-23 12:28:45 +0200

    add

---
+++ Assets/HandleDoor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleDoor : MonoBehaviour
+{
+    private float RotationZ;
+    public GameObject Door;
+    public Material greenMat;
+    private Quaternion targetRotation;
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = Door.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotationZ = transform.localEulerAngles.z;
+        if(RotationZ >= 90)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            Door.GetComponent<MeshRenderer>().material = greenMat;
+            targetRotation = Quaternion.AngleAxis(90, transform.up);
+            Door.transform.rotation = Quaternion.Lerp(Door.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        }
+
+        
+    }
+}
