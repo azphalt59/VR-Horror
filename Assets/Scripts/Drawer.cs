4b6188d082e24c4c3d903cb59e1515e02f731018
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-09-30 14:55:29 +0200

    dd

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+    [SerializeField] public Transform DrawerOpenPosition;
+    public void DrawerPosition()
+    {
+
+    }
+}
