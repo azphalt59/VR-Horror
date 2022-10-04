c62c324fd55f72ceaa2f4887fdf7131ee19fc0f1
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-10-04 15:03:16 +0200

    des p'tit trucs à tester à la maison

--- Assets/Scripts/VR/Drawer.cs
+++ Assets/Scripts/VR/Drawer.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Drawer : MonoBehaviour
 {
-    [SerializeField] public Transform DrawerOpenPosition;
-    public void DrawerPosition()
+    public float Open_PositionZ;
+    
+    private void Update()
     {
-
+        
+        if(transform.localPosition.z >= Open_PositionZ)
+        {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+        if(transform.localPosition.z <= Open_PositionZ)
+        {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+        }
     }
 }
