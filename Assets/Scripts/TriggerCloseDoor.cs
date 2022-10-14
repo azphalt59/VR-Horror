9f29ec41a46b9d50fea4f8824c5ba7428b61b730
azphalt59 <62052602+azphalt59@users.noreply.github.com>
2022-10-14 10:07:25 +0200

    xxx

--- Assets/Scripts/TriggerCloseDoor.cs
+++ Assets/Scripts/TriggerCloseDoor.cs
@@ -5,20 +5,61 @@ using UnityEngine;
 public class TriggerCloseDoor : MonoBehaviour
 {
     public HandleDoor handleDoorScript;
+    public bool HingeDoor = false;
+    public GameObject Door;
+    public float RotationSpeed;
+    private float DoorRotationY;
+    public GameObject FrontTrigger;
+    public GameObject BehindTrigger;
+    private bool canClose = false;
     // Start is called before the first frame update
     void Start()
     {
         //Destroy(GetComponent<MeshFilter>());
         //Destroy(GetComponent<MeshRenderer>());
+        DoorRotationY = Door.transform.localEulerAngles.y;
     }
     private void OnTriggerExit(Collider other)
     {
         if(other.tag == "Player")
         {
-            
-            handleDoorScript.CloseDoorBoolean();
+            if(HingeDoor == false)
+            {
+                handleDoorScript.CloseDoorBoolean();
+            }
+            if(HingeDoor == true)
+            {
+                canClose = true;
+                FrontTrigger.SetActive(false);
+                FrontTrigger.SetActive(false);
+            }
         }
     }
+    private void Update()
+    {
+        if(canClose == true)
+        {
+            CloseTheDoor();
+        }
+        
+    }
+    public void CloseTheDoor()
+    {
+        if(DoorRotationY < Door.transform.localEulerAngles.y -1)
+        {
+            Door.transform.localEulerAngles += new Vector3(0, 1, 0) * Time.deltaTime * RotationSpeed;
+        }
+        if (DoorRotationY > Door.transform.localEulerAngles.y +1)
+        {
+            Door.transform.localEulerAngles -= new Vector3(0, 1, 0) * Time.deltaTime * RotationSpeed;
+        }
+        if(DoorRotationY <= Door.transform.localEulerAngles.y -1 || DoorRotationY >= Door.transform.localEulerAngles.y +1)
+        {
+            canClose = false;
+        }
+
+    }
+
 
 
 }
